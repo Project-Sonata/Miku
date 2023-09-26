cfc36b6fb6e0465a4e7bce4656db91e2ea51ffb9
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-26 22:15:34 +0300

    Root files

---
+++ src/test/java/com/odeyalo/sonata/miku/MikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.miku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
