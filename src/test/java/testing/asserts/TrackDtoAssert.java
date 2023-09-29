015790c9d5de42a167e7f6e60a6c8f1f5c746b26
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-29 17:45:44 +0300

    Asserts for TrackDto

---
+++ src/test/java/testing/asserts/TrackDtoAssert.java
@@ -0,0 +1,45 @@
+package testing.asserts;
+
+import com.odeyalo.sonata.miku.dto.TrackDto;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.AbstractStringAssert;
+
+public class TrackDtoAssert extends AbstractAssert<TrackDtoAssert, TrackDto> {
+
+    public TrackDtoAssert(TrackDto actual) {
+        super(actual, TrackDtoAssert.class);
+    }
+
+    public static TrackDtoAssert forTrack(TrackDto actual) {
+        return new TrackDtoAssert(actual);
+    }
+
+    public IdAssert id() {
+        return new IdAssert(actual.getId());
+    }
+
+    public TrackNameAssert name() {
+        return TrackNameAssert.forTrackName(actual.getName(), this);
+    }
+
+    public TrackDurationAssert duration() {
+        return new TrackDurationAssert(actual.getDurationMs());
+    }
+
+    public static final class TrackNameAssert extends AbstractStringAssert<TrackNameAssert> {
+        private final TrackDtoAssert parent;
+
+        public TrackNameAssert(String actual, TrackDtoAssert parent) {
+            super(actual, TrackNameAssert.class);
+            this.parent = parent;
+        }
+
+        public static TrackNameAssert forTrackName(String actual, TrackDtoAssert parent) {
+            return new TrackNameAssert(actual, parent);
+        }
+
+        public TrackDtoAssert and() {
+            return parent;
+        }
+    }
+}
