c3c2ea9cdfc0b6fe0a9b18ba2bfd1fbd000efc54
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-29 17:45:22 +0300

    Dto to transfer Track info

---
+++ src/main/java/com/odeyalo/sonata/miku/dto/TrackDto.java
@@ -0,0 +1,20 @@
+package com.odeyalo.sonata.miku.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * Dto to transfer Track info
+ */
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TrackDto {
+    String id;
+    String name;
+    @JsonProperty("duration_ms")
+    Long durationMs;
+}
