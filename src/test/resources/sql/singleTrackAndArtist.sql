0301807b4716e38a984a169820dbc7c09081ad9d
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-04 13:30:54 +0300

    sql scripts

--- src/test/resources/sql/singleTrackAndArtist.sql
+++ src/test/resources/sql/singleTrackAndArtist.sql
@@ -2,9 +2,14 @@ WITH artist_insert_id AS (
     insert into artists (public_id, artist_name)
         values ('artistuniqueid', 'Mosquit') returning id),
 
+     album_insert_id AS (
+         insert into albums (public_id, album_name, album_type)
+             values ('albumid', 'Adobe of the soul', 'ALBUM') returning id),
+
      track_insert_id AS (
-         insert into tracks (public_id, name, duration_ms)
-             values ('trackuniqueid', 'Chill', 1200) returning id)
+         insert into tracks (public_id, name, duration_ms, album_id)
+             select 'trackuniqueid', 'Chill', 1200, album_insert_id.id FROM album_insert_id returning id)
+
 
 INSERT INTO tracks_artists (track_id, artist_id)
 SELECT track_insert_id.id, artist_insert_id.id
