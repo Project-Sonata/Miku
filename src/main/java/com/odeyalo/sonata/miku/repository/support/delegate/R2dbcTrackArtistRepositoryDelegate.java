daedc19b2ce180020df9dad61e311d3fba8fb66a
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-05 22:39:52 +0300

    R2DBC specific repository to work with TrackArtistEntity

---
+++ src/main/java/com/odeyalo/sonata/miku/repository/support/delegate/R2dbcTrackArtistRepositoryDelegate.java
@@ -0,0 +1,22 @@
+package com.odeyalo.sonata.miku.repository.support.delegate;
+
+import com.odeyalo.sonata.miku.entity.TrackArtistEntity;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * R2DBC specific repository to work with TrackArtistEntity
+ */
+@Repository
+public interface R2dbcTrackArtistRepositoryDelegate extends R2dbcRepository<TrackArtistEntity, Long> {
+
+    Mono<Void> deleteAllByTrackId(Long trackId);
+
+    Mono<Void> deleteAllByArtistId(Long artistId);
+
+    Flux<TrackArtistEntity> findAllByTrackId(Long trackId);
+
+    Flux<TrackArtistEntity> findAllByArtistId(Long artistId);
+}
