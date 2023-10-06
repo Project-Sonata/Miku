df6ac417dd68ef974063842a78e33c45fab1d98c
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-06 14:47:10 +0300

    Now use SuperBuilder instead of Builder

--- src/main/java/com/odeyalo/sonata/miku/entity/SimplifiedAlbumEntity.java
+++ src/main/java/com/odeyalo/sonata/miku/entity/SimplifiedAlbumEntity.java
@@ -1,8 +1,12 @@
 package com.odeyalo.sonata.miku.entity;
 
 import com.odeyalo.sonata.miku.model.AlbumType;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -10,7 +14,7 @@ import org.springframework.data.relational.core.mapping.Table;
 @Data
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Table("albums")
 public class SimplifiedAlbumEntity {
