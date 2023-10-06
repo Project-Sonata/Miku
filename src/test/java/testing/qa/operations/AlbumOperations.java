74d7ecef4813644731a41f6a3ce057f872aeddbd
odeyalo <theprogrammerinfuture@gmail.com>
2023-10-06 14:44:58 +0300

    Added operation to save the AlbumEntity

--- src/test/java/testing/qa/operations/AlbumOperations.java
+++ src/test/java/testing/qa/operations/AlbumOperations.java
@@ -1,10 +1,13 @@
 package testing.qa.operations;
 
+import com.odeyalo.sonata.miku.entity.AlbumEntity;
 import com.odeyalo.sonata.miku.entity.SimplifiedAlbumEntity;
 
 public interface AlbumOperations {
 
     SimplifiedAlbumEntity saveSimplifiedAlbum(SimplifiedAlbumEntity album);
+
+    AlbumEntity saveAlbum(AlbumEntity album);
 
     void clear();
 }
