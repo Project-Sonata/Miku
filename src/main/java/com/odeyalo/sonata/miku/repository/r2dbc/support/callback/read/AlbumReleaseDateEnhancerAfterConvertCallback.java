6e55c6c9cf391d637f67415b301d958574317b33
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-12 17:33:37 +0200

    Callback that invoked after the SimplifiedAlbumEntity converted and used to set ReleaseDate

---
+++ src/main/java/com/odeyalo/sonata/miku/repository/r2dbc/support/callback/read/AlbumReleaseDateEnhancerAfterConvertCallback.java
@@ -0,0 +1,46 @@
+package com.odeyalo.sonata.miku.repository.r2dbc.support.callback.read;
+
+import com.odeyalo.sonata.miku.entity.SimplifiedAlbumEntity;
+import com.odeyalo.sonata.miku.model.ReleaseDate;
+import com.odeyalo.sonata.miku.model.ReleaseDate.Precision;
+import com.odeyalo.sonata.miku.repository.r2dbc.support.release.release.ReleaseDateDecoder;
+import com.odeyalo.sonata.miku.repository.r2dbc.support.release.release.ReleaseDateRowInfo;
+import org.apache.commons.lang3.EnumUtils;
+import org.jetbrains.annotations.NotNull;
+import org.reactivestreams.Publisher;
+import org.springframework.data.r2dbc.mapping.event.AfterConvertCallback;
+import org.springframework.data.relational.core.sql.SqlIdentifier;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * Callback that invoked after the SimplifiedAlbumEntity converted and used to set ReleaseDate
+ */
+@Component
+public class AlbumReleaseDateEnhancerAfterConvertCallback implements AfterConvertCallback<SimplifiedAlbumEntity> {
+    private final ReleaseDateDecoder<ReleaseDateRowInfo> releaseDateDecoder;
+
+    public AlbumReleaseDateEnhancerAfterConvertCallback(ReleaseDateDecoder<ReleaseDateRowInfo> releaseDateDecoder) {
+        this.releaseDateDecoder = releaseDateDecoder;
+    }
+
+    @Override
+    @NotNull
+    public Publisher<SimplifiedAlbumEntity> onAfterConvert(@NotNull SimplifiedAlbumEntity entity,
+                                                           @NotNull SqlIdentifier table) {
+
+        String releaseDate = entity.getReleaseDateAsString();
+        Precision precision = toPrecision(entity);
+
+        ReleaseDate date = releaseDateDecoder.decodeReleaseDate(ReleaseDateRowInfo.of(releaseDate, precision));
+
+        entity.setReleaseDate(date);
+
+        return Mono.just(entity);
+    }
+
+    @NotNull
+    private static Precision toPrecision(@NotNull SimplifiedAlbumEntity entity) {
+        return EnumUtils.getEnum(Precision.class, entity.getReleaseDatePrecision());
+    }
+}
