e5212459561a30b7e89b0d595b46e56b6e8c153c
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-12 18:19:52 +0200

    Added release date

--- src/test/java/testing/faker/SimplifiedAlbumEntityFaker.java
+++ src/test/java/testing/faker/SimplifiedAlbumEntityFaker.java
@@ -3,6 +3,7 @@ package testing.faker;
 import com.github.javafaker.Faker;
 import com.odeyalo.sonata.miku.entity.SimplifiedAlbumEntity;
 import com.odeyalo.sonata.miku.model.AlbumType;
+import com.odeyalo.sonata.miku.model.ReleaseDate;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class SimplifiedAlbumEntityFaker {
@@ -13,7 +14,8 @@ public class SimplifiedAlbumEntityFaker {
         builder.publicId(RandomStringUtils.randomAlphanumeric(22))
                 .name(faker.name().title())
                 .albumType(faker.options().option(AlbumType.class))
-                .totalTracksCount(faker.random().nextInt(0, 10));
+                .totalTracksCount(faker.random().nextInt(0, 10))
+                .releaseDate(ReleaseDateFaker.randomReleaseDate().get());
     }
 
     public static SimplifiedAlbumEntityFaker create() {
@@ -37,6 +39,11 @@ public class SimplifiedAlbumEntityFaker {
 
     public SimplifiedAlbumEntityFaker setTotalTracks(int totalTracks) {
         this.builder.totalTracksCount(totalTracks);
+        return this;
+    }
+
+    public SimplifiedAlbumEntityFaker releaseDate(ReleaseDate releaseDate) {
+        builder.releaseDate(releaseDate);
         return this;
     }
 
