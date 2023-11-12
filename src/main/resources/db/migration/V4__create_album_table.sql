bfec4784fed4107a93a7d32078855c9d0a66912a
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-12 17:04:52 +0200

    Added release_date and release_date_precision

--- src/main/resources/db/migration/V4__create_album_table.sql
+++ src/main/resources/db/migration/V4__create_album_table.sql
@@ -1,7 +1,9 @@
 create table albums
 (
-  id SERIAL PRIMARY KEY,
-  public_id VARCHAR(255) NOT NULL UNIQUE,
-  album_name VARCHAR(255) NOT NULL,
-  album_type VARCHAR(50) NOT NULL
+    id                     SERIAL PRIMARY KEY,
+    public_id              VARCHAR(255) NOT NULL UNIQUE,
+    album_name             VARCHAR(255) NOT NULL,
+    album_type             VARCHAR(50)  NOT NULL,
+    release_date           VARCHAR(100) NOT NULL,
+    release_date_precision VARCHAR(50)  NOT NULL
 );
