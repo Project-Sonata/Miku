0d42073c05d204408dcc8ae85b7b9ed2e8f401aa
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-07 21:06:00 +0200

    Now using custom JsonDeserializer

--- src/main/java/com/odeyalo/sonata/miku/config/KafkaConfiguration.java
+++ src/main/java/com/odeyalo/sonata/miku/config/KafkaConfiguration.java
@@ -1,5 +1,7 @@
 package com.odeyalo.sonata.miku.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.odeyalo.sonata.miku.support.kafka.HeadersPopulatorJsonTypeResolver;
 import com.odeyalo.sonata.suite.brokers.events.SonataEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
+import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import reactor.kafka.receiver.ReceiverOptions;
 
@@ -18,7 +22,8 @@ import java.util.Map;
 public class KafkaConfiguration {
 
     @Bean
-    public ReceiverOptions<String, SonataEvent> albumEventWarehouseKafkaReceiver(KafkaProperties props) {
+    public ReceiverOptions<String, SonataEvent> albumEventWarehouseKafkaReceiver(KafkaProperties props,
+                                                                                 JsonDeserializer<SonataEvent> jsonDeserializer) {
         Map<String, Object> consumerProps = new HashMap<>();
         consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, props.getBootstrapServers());
         consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, props.getConsumer().getGroupId());
@@ -27,7 +32,27 @@ public class KafkaConfiguration {
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
         return ReceiverOptions.<String, SonataEvent>create(consumerProps)
+                .withValueDeserializer(jsonDeserializer)
                 .subscription(Collections.singleton("albums-event-warehouse"));
+    }
+
+
+    @Bean
+    public JsonDeserializer<SonataEvent> sonataEventJsonDeserializer(ObjectMapper objectMapper,
+                                                                     Jackson2JavaTypeMapper mapper,
+                                                                     EventTypeProviderContainer container) {
+
+        JsonDeserializer<SonataEvent> deserializer = new JsonDeserializer<>(objectMapper);
+        deserializer.trustedPackages("*");
+        deserializer.setTypeResolver(new HeadersPopulatorJsonTypeResolver(mapper, container));
+        return deserializer;
+    }
+
+    @Bean
+    public Jackson2JavaTypeMapper jackson2JavaTypeMapper() {
+        DefaultJackson2JavaTypeMapper mapper = new DefaultJackson2JavaTypeMapper();
+        mapper.addTrustedPackages("*");
+        return mapper;
     }
 
     @Bean
