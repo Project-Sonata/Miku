239744967b11e24955f7dd27f70449beab55ba4f
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-08 22:14:47 +0200

    Added streaming uri to track

--- src/main/resources/db/migration/V1__create_track_table.sql
+++ src/main/resources/db/migration/V1__create_track_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE tracks
 (
-  id SERIAL PRIMARY KEY,
-  public_id varchar(250) NOT NULL UNIQUE,
-  name varchar(255) NOT NULL,
-  duration_ms BIGINT NOT NULL
+    id            SERIAL PRIMARY KEY,
+    public_id     varchar(250)  NOT NULL UNIQUE,
+    name          varchar(255)  NOT NULL,
+    duration_ms   BIGINT        NOT NULL,
+    streaming_uri varchar(3000) NOT NULL UNIQUE
 );
