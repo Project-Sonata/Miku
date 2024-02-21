717e7262c7c0062dad920d67399322cabfdbf89e
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-21 17:21:52 +0200

    Added images to converter

--- src/main/java/com/odeyalo/sonata/miku/support/converter/AlbumDtoConverter.java
+++ src/main/java/com/odeyalo/sonata/miku/support/converter/AlbumDtoConverter.java
@@ -9,13 +9,16 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring",
         uses = {
                 ArtistsDtoConverter.class,
-                SimplifiedTrackDtoContainerConverter.class
+                SimplifiedTrackDtoContainerConverter.class,
+                ImageDtoContainerConverter.class
         })
 public interface AlbumDtoConverter {
 
     @Mapping(target = "id", source = "publicId")
+    @Mapping(target = "images", source = "imageEntities")
     AlbumDto toAlbumDto(SimplifiedAlbumEntity entity);
 
     @Mapping(target = "id", source = "publicId")
+    @Mapping(target = "images", source = "imageEntities")
     AlbumDto toAlbumDto(AlbumEntity entity);
 }
