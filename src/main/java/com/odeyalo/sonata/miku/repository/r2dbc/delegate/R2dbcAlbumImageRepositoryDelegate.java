092fc453ff9088e7afdad174b8dbfb42a5febb0e
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-21 17:15:06 +0200

    Used as delegate by R2dbcAlbumImageRepository

---
+++ src/main/java/com/odeyalo/sonata/miku/repository/r2dbc/delegate/R2dbcAlbumImageRepositoryDelegate.java
@@ -0,0 +1,21 @@
+package com.odeyalo.sonata.miku.repository.r2dbc.delegate;
+
+import com.odeyalo.sonata.miku.entity.AlbumImageEntity;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Used as delegate by {@link com.odeyalo.sonata.miku.repository.r2dbc.R2dbcAlbumImageRepository}
+ */
+@Repository
+public interface R2dbcAlbumImageRepositoryDelegate extends R2dbcRepository<AlbumImageEntity, Long> {
+
+    @NotNull
+    Mono<AlbumImageEntity> findByAlbumId(long albumId);
+
+    @NotNull
+    Flux<AlbumImageEntity> findAllByAlbumId(long albumId);
+}
