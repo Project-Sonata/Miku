6faf500c168b75b00f93d764ba6d90d469a0c585
odeyalo <theprogrammerinfuture@gmail.com>
2024-02-22 15:32:34 +0200

    Fixed bug when AfterSaveCallback was used instead of AfterConvertCallback

--- src/main/java/com/odeyalo/sonata/miku/repository/r2dbc/support/callback/read/AlbumImageEnhancerAfterConvertCallback.java
+++ src/main/java/com/odeyalo/sonata/miku/repository/r2dbc/support/callback/read/AlbumImageEnhancerAfterConvertCallback.java
@@ -6,8 +6,7 @@ import com.odeyalo.sonata.miku.repository.AlbumImageRepository;
 import org.jetbrains.annotations.NotNull;
 import org.reactivestreams.Publisher;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.r2dbc.mapping.OutboundRow;
-import org.springframework.data.r2dbc.mapping.event.AfterSaveCallback;
+import org.springframework.data.r2dbc.mapping.event.AfterConvertCallback;
 import org.springframework.data.relational.core.sql.SqlIdentifier;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +14,7 @@ import org.springframework.stereotype.Component;
  * Populate the converted {@link SimplifiedAlbumEntity} with cover images
  */
 @Component
-public final class AlbumImageEnhancerAfterConvertCallback implements AfterSaveCallback<SimplifiedAlbumEntity> {
+public final class AlbumImageEnhancerAfterConvertCallback implements AfterConvertCallback<SimplifiedAlbumEntity> {
     private final AlbumImageRepository albumImageRepository;
 
     public AlbumImageEnhancerAfterConvertCallback(@Lazy AlbumImageRepository albumImageRepository) {
@@ -24,14 +23,12 @@ public final class AlbumImageEnhancerAfterConvertCallback implements AfterSaveCa
 
     @Override
     @NotNull
-    public Publisher<SimplifiedAlbumEntity> onAfterSave(@NotNull SimplifiedAlbumEntity albumEntity,
-                                                        @NotNull OutboundRow outboundRow,
-                                                        @NotNull SqlIdentifier table) {
+    public Publisher<SimplifiedAlbumEntity> onAfterConvert(@NotNull SimplifiedAlbumEntity albumEntity,
+                                                           @NotNull SqlIdentifier table) {
 
         return albumImageRepository.findAllByAlbumId(albumEntity.getId())
                 .collectList()
                 .doOnNext(albumImageEntities -> albumEntity.setImages(ImageEntityContainer.fromCollection(albumImageEntities)))
-                .thenReturn(albumEntity)
-                .log("image-enhancer");
+                .thenReturn(albumEntity);
     }
 }
